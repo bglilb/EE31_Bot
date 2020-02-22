7886855c27f12f087b1da058fb9d6252b8838a59
Sean Moushegian <moushegian@users.noreply.github.com>
2020-02-22 17:57:33 -0500

    Separated out control for left vs right wheels

--- Phase 2B/SimpleDrive.ino
+++ Phase 2B/SimpleDrive.ino
@@ -30,26 +30,29 @@ void loop() {
     Serial.println(msg + speedOutput);
 
   }
-  drive_loop(speedOutput);
+  drive_loop(speedOutput, speedOutput);
 }
 
-void drive_loop(int output){
-  if (output >= 0) {
-    // Wheel A
+void drive_loop(int leftOutput, int rightOutput){
+  if (leftOutput >= 0) {
+    // Left Wheel
     digitalWrite(HBRIDGE_2, LOW);
-    analogWrite(HBRIDGE_1, output);
-
-    // Wheel B
-    digitalWrite(HBRIDGE_4, LOW);
-    analogWrite(HBRIDGE_3, output);
+    analogWrite(HBRIDGE_1, leftOutput);
   }
   else {
-    // Wheel A
+    // Left Wheel
     digitalWrite(HBRIDGE_1, LOW);
-    analogWrite(HBRIDGE_2, abs(output));
+    analogWrite(HBRIDGE_2, abs(leftOutput));    
+  }
 
-    // Wheel B
+  if (rightOutput >= 0) {
+    // Right Wheel
+    digitalWrite(HBRIDGE_4, LOW);
+    analogWrite(HBRIDGE_3, rightOutput);
+  }
+  else {
+    // Right Wheel
     digitalWrite(HBRIDGE_3, LOW);
-    analogWrite(HBRIDGE_4, abs(output));
+    analogWrite(HBRIDGE_4, abs(rightOutput));
   }
 }
